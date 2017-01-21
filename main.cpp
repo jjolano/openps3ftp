5f0a70c41e6ee4704a7106b4e198b8fb7feeafbe
jjolano <jjolano@me.com>
2017-01-20 19:13:47 -0500

    Restructuring (and fixed Makefile again)

--- main.cpp
+++ main.cpp
@@ -1,13 +1,3 @@
-/*
- * main.cpp: screen+input handling and ftp server bootstrap
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * <jjolano@me.com> wrote this file. As long as you retain this notice you
- * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return John Olano
- * ----------------------------------------------------------------------------
- */
-
 #include <cstdlib>
 
 #include <NoRSX.h>
