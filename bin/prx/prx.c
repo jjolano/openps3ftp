8e9fa7b108f9de0d3635422b9ebf6a51da0caf7d
jjolano <jjolano@me.com>
2017-03-20 08:42:21 -0400

    change ntfs mount name convention

--- bin/prx/prx.c
+++ bin/prx/prx.c
@@ -114,7 +114,7 @@ void ntfs_main(uint64_t ptr)
 						for(j = 0; j < num_partitions; j++)
 						{
 							char name[32];
-							sprintf(name, "ntfs%d-%d", i, j);
+							sprintf(name, "ntfs%d_usb%03d", j, i);
 
 							if(ntfsMount(name, ntfs_usb_if[i], partitions[j], CACHE_DEFAULT_PAGE_COUNT, CACHE_DEFAULT_PAGE_SIZE, NTFS_FORCE))
 							{
